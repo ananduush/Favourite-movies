59f68e935c689528d64953973081453b66c64522
ananduush <vintagevyector@gmail.com>
2022-05-21 20:04:45 +0800

    Added sorting functions

--- script.js
+++ script.js
@@ -20,6 +20,28 @@ function addMovie() {
 
 function deleteMovie(ele) {
     let tr = ele.parentNode.parentNode;
-    let tb = tr.parentNode
-    tb.removeChild(tr)
+    let tb = tr.parentNode;
+    tb.removeChild(tr);
+}
+
+
+// var td = document.querySelectorAll("td");
+// console.log(td[3].innerHTML)
+
+function sortData() {
+    var movieArray = [];
+    var tds = Array.from(document.querySelectorAll("#myTable tr td"));
+
+    for (let i = 0; i < tds.length; i += 3) {
+        movieArray.push({ title: tds[i].innerHTML, rating: tds[i + 1].innerHTML });
+    }
+
+    var byTitle = movieArray.slice(0);
+    byTitle.sort(function(a, b) {
+        var x = a.title.toLowerCase();
+        var y = b.title.toLowerCase();
+        return x < y ? -1 : x > y ? 1 : 0;
+    });
+
+    console.log(byTitle);
 }
